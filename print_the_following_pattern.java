345f62d5e53e70cc903f6c5554bbaec38aea524e
aishwaryabodem <106659972+aishwaryabodem@users.noreply.github.com>
2022-09-12 02:52:04 +0530

    import java.util.*; class Pattern {     public static void main(String args[])     {         int i,j,n;         Scanner sc=new Scanner(System.in);         //System.out.println("Enter the no of lines");         n=sc.nextInt();         for(i=n;i>=1;i--)         {             for(j=1;j<=i;j++)             {                 System.out.print((char)(i+64)+" ");             }             System.out.println("");         }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -5,15 +5,15 @@ class Pattern
     {
         int i,j,n;
         Scanner sc=new Scanner(System.in);
-        //System.out.println(5);
+        //System.out.println("Enter the no of lines");
         n=sc.nextInt();
-        for(i=1;i<=n;i++)
+        for(i=n;i>=1;i--)
         {
-            for(j=1;j<=n;j++)
+            for(j=1;j<=i;j++)
             {
                 System.out.print((char)(i+64)+" ");
             }
-            System.out.println();
+            System.out.println("");
         }
     }
 }
