00e86c76d5e0f7910b835ad9ffd39707d828f0db
aishwaryabodem <106659972+aishwaryabodem@users.noreply.github.com>
2022-11-05 15:03:18 +0530

    import java.util.Scanner; class ReverseNumber { public static void main(String[] args) {     Scanner sc=new Scanner(System.in); int number =sc.nextInt(); int reverse = 0; while(number != 0) { int remainder = number % 10; reverse = reverse * 10 + remainder; number = number/10; } System.out.println(reverse); } }

--- Reverse_Integer.java
+++ Reverse_Integer.java
@@ -1,23 +1,17 @@
 import java.util.Scanner;
-class Sample
-{
-    public static int findReverse(int n)
-    {
-        int sum=0,r;
-        while(n>0||n<0)
-        {
-            r=n%10;
-            sum=sum*10+r;
-            n=n/10;
-        }
-        return sum;
-    }
-    public static void main (String args[])
-    {
-        Scanner sc=new Scanner(System.in);
-        int n,res;
-        n=sc.nextInt();
-        res=findReverse(n);
-        System.out.println(res);
-    }
-}
+class ReverseNumber  
+{  
+public static void main(String[] args)   
+{  
+    Scanner sc=new Scanner(System.in);
+int number =sc.nextInt();
+int reverse = 0;  
+while(number != 0)   
+{  
+int remainder = number % 10;  
+reverse = reverse * 10 + remainder;  
+number = number/10;  
+}  
+System.out.println(reverse);  
+}  
+}  
