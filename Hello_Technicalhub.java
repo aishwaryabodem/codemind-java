b9475f0bbf680d5d46c85281bbc3cbcefe51978c
aishwaryabodem <106659972+aishwaryabodem@users.noreply.github.com>
2023-01-18 22:19:07 +0530

    import java.util.Scanner; class Tech{     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String s1="Hello Technicalhub";         System.out.println(s1);         System.out.print("");         String str=sc.nextLine();         System.out.println(str);
        }
    }

---
+++ Hello_Technicalhub.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Tech{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s1="Hello Technicalhub";
+        System.out.println(s1);
+        System.out.print("");
+        String str=sc.nextLine();
+        System.out.println(str);
+      
+    }
+}
